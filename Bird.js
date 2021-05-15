6fcc3bacc69a1d08c321d3f0d3bbe2707182695e
Saksham989 <75685036+Saksham989@users.noreply.github.com>
2021-05-15 16:46:56 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,10 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,100,100);
+    //this.image = loadImage("sprites/bird.png");
+    //this.smokeImage = loadImage("sprites/smoke.png");
+    //this.trajectory =[];
+  }
+
+}
+
